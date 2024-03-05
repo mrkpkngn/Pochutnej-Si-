abca6bde0e41ab13753deb7853a12f79b954a2aa
mrkpkngn <mrk.pkngn@gmail.com>
2024-03-05 12:27:58 +0800

    first commit

---
+++ pochutnejsi-website/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <img src="./img/pochutnejsi_logo.png"></img>
+    </div>
+  );
+}
+
+export default App;
