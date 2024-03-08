43e388d6d2c7499ba2df9b5880ffea634f764ec0
mrkpkngn <mrk.pkngn@gmail.com>
2024-03-08 16:42:14 +0800

    fixed one card per swipe in gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -21,7 +21,7 @@ function Gallery() {
 
             <div className="p-10 pb-24 flex flex-col items-center gap-6 bg-yellow w-screen">
             <h2 className="font-body font-bold text-white text-3xl text-center md:text-5xl">Cateringy - Svatby - Oslavy - Vecírky</h2>
-            <Swiper effect={"cards"} modules={[EffectCards]} className="catering h-[320px] w-[240px] md:h-[480px] md:w-[360px]" nRealIndexChange={(swiper) => {swiper.allowTouchMove = false;swiper.unsetGrabCursor();}} onTouchEnd={(swiper) => {swiper.allowTouchMove = true;}}>
+            <Swiper effect={"cards"} modules={[EffectCards]} className="catering h-[320px] w-[240px] md:h-[480px] md:w-[360px]" onRealIndexChange={(swiper) => {swiper.allowTouchMove = false;swiper.unsetGrabCursor();}} onTouchEnd={(swiper) => {swiper.allowTouchMove = true;}}>
                 <SwiperSlide className="rounded-3xl"></SwiperSlide>
                 <SwiperSlide className="rounded-3xl"></SwiperSlide>
                 <SwiperSlide className="rounded-3xl"></SwiperSlide>
