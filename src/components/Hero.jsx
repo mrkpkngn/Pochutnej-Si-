87a9a2cef288d2d2e3bcf147c77dd78a1545bb0b
mrkpkngn <mrk.pkngn@gmail.com>
2024-03-14 00:18:41 +0800

    fixed hero bg transition?

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ import "./hero.css"
 
     return (
         <div className="flex flex-col justify-center items-center z-10 py-28 md:pb-28 hero"> 
-        <div className="block h-fit z-10"><img src={Heroimg} alt="Hero Image" className="pt-10 pb-10 md:w-[65rem] md:pt-[7rem] md:pb-[1rem] z-10"></img></div>
+        <div className="block h-fit z-10"><img src={Heroimg} alt="Hero Image" className="pt-10 pb-10 md:w-[65rem] md:pt-[3rem] md:pb-[1rem] z-10"></img></div>
         <div className='flex gap-6'>
            <Link to='https://pochutnej-si.choiceqr.com/takeaway' className={`font-header xl:text-2xl card1 hover:border-2 z-0 hidden text-center p-5 hover:text-white text-orange hover:bg-orange bg-white rounded-xl  lg:flex flex-col`}>OBĚD S SEBOU
           <p className=" font-body text-sm lg:text-xs ">PONDĚLÍ AŽ PÁTEK OD 11 DO 14 HODIN</p></Link>
@@ -38,6 +38,9 @@ import "./hero.css"
           <p className="font-body text-sm ">UŽIJTE SI OSLAVU ČI VEČÍREK BEZ STAROSTÍ!</p></Link>
           <Link to='https://www.pochutnejsi.cz/Darkovy-poukaz-BISTRO-500-d3123.htm' className={`font-header xl:text-2xl hover:border-2 hidden text-center p-5 hover:text-white text-orange hover:bg-orange bg-white rounded-xl lg:flex flex-col`}>DÁRKOVÝ POUKAZ</Link>
         </div>
+
+        <FaChevronDown className="hidden md:flex md:text-white md:text-3xl md:mt-12 md:animate-bounce"></FaChevronDown>
+
         <div className="flex flex-col gap-3 lg:hidden" id="anchor">
         <Link to='https://pochutnej-si.choiceqr.com/takeaway' className="bg-white p-3 md:p-4 text-orange text-sm md:text-lg rounded-md font-header flex flex-col text-center hover:text-white hover:bg-orange hover:border-2" data-aos="fade-left" data-aos-duration="1000" data-aos-anchor="#anchor">
           OBĚD S SEBOU <span className="text-xs md:text-sm font-body">PONDĚLÍ AŽ PÁTEK OD 11 DO 14 HODIN</span>
